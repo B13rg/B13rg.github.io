b79f881db20363aa330ec0ad6787d6609dd951dc
Yuki <yuki@bushi.moe>
2025-06-10 13:04:22 -0700

    initial text extract

--- files/soviet-factories/processing/pdf2img.py
+++ files/soviet-factories/processing/pdf2img.py
@@ -1,7 +1,15 @@
 from pdf2image import convert_from_path
+import pytesseract
+# pip install --user pdf2img pytesseract
+# brew install tesseract
 
 filename = "CIA-RDP82-00047R000100160001-1"
 
-images = convert_from_path("./"+filename+".pdf", dpi=300)
+images = convert_from_path("./"+filename+".pdf", dpi=600)
 for i, image in enumerate(images):
     image.save(f"{filename}_output_{i}.png", "PNG")
+    image = image.convert("L")  # Grayscale
+    image = image.point(lambda p: p > 128 and 255)  # Binary threshold
+    text = pytesseract.image_to_string(image, lang='eng')
+    with open(f"{filename}_text_{i}.txt", "w") as f:
+        f.write(text)
